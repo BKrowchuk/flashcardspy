24d90dee28558bab5163a6c8f90a632f88fac74f
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-06-05 16:47:09 -0400

    adding submit button

--- CreateNewDeck.py
+++ CreateNewDeck.py
@@ -1,6 +1,7 @@
 # All the code for creating a new deck
 import os
 import tkinter as tk
+from functools import partial
 
 
 def print_hi():
@@ -8,12 +9,23 @@ def print_hi():
 
 
 def create_new_deck(frame):
-    subtitle = tk.Label(frame, text="Create New Deck", pady=5, padx=10, bg="light blue")
+    deck_frame = tk.Frame(frame, bg="light blue")
+    deck_frame.place(relwidth=0.9, relx=0.05, rely=0.05)
+
+    subtitle = tk.Label(deck_frame, text="Create New Deck", pady=5, padx=10, bg="light blue")
     subtitle.pack()
 
-    deck_name_lbl = tk.Label(frame, text="Deck Name: ", pady=5, padx=10, bg="light blue")
-    deck_name_lbl.pack()
+    deck_name_lbl = tk.Label(deck_frame, text="Deck Name: ", pady=5, padx=10, bg="light blue")
+    deck_name_lbl.pack(side="left")
 
-    submit_btn = tk.Button(frame, text="Submit", padx=10, pady=5, width=10,
-                                    fg="white", bg="#263D42", command=print_hi)
-    submit_btn.pack(pady=3)
+    text_field = tk.Entry(deck_frame)
+    text_field.pack(side="left")
+    text_field.focus_set()
+
+    submit_btn = tk.Button(deck_frame, text="Submit", padx=10, pady=0, width=7,
+                           fg="white", bg="#263D42", command=lambda: submit_name(text_field.get()))
+    submit_btn.pack(pady=3, side="bottom")
+
+
+def submit_name(name):
+    print(name)
